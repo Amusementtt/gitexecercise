4c7a5138d61c0b58abd9f5d7aa859b33246c2b26
wangyu <wangyu9@bonc.com.cn>
2021-01-12 11:06:15 +0800

    最后修改

--- src/main/java/com/wangyu/git/GitApplication.java
+++ src/main/java/com/wangyu/git/GitApplication.java
@@ -11,10 +11,6 @@ public class GitApplication {
 		System.out.println("111");
 		System.out.println("222");
 		System.out.println("333");
-		System.out.println("444");
-		System.out.println("555");
-		System.out.println("666");
-		System.out.println("777");
 	}
 
 }
